0a1978cb595023e33e5ce3ff9e4c67f8e2021d3e
Renato Manuel Branco Capelo Ventura <2219112@alunos.escoladigital.com>
2022-06-08 03:08:09 +0100

    feat - update rating controller

--- AppStoreAPI/Controllers/AppRatingsController.cs
+++ AppStoreAPI/Controllers/AppRatingsController.cs
@@ -62,6 +62,7 @@ namespace AppStoreAPI.Controllers
                 return Ok(ratings);
             }
         }
+
         [HttpPost("{guid}")]
         [Authorize]
         public async Task<IActionResult> postRating([FromRoute] Guid guid,[FromBody] ApplicationRating_ToPost comment)
@@ -81,6 +82,32 @@ namespace AppStoreAPI.Controllers
                     idApplication = idApp
                 };
                 await con.InsertAsync(rating);
+                con.Close();
+            }
+            return Ok();
+        }
+
+        [HttpPut("{guid}")]
+        [Authorize]
+        public async Task<IActionResult> updateRating([FromRoute] Guid guid, [FromBody] ApplicationRating_ToPost comment)
+        {
+            var userGuid = Guid.Parse(User.FindFirst("Guid").Value);
+
+            using (var con = new SqlConnection(config.GetConnectionString("DefaultConnection")))
+            {
+                con.Open();
+                var idUser = await con.QueryFirstAsync<int>("SELECT id FROM Users where guid=@userGuid", new { userGuid });
+                var idApp = await con.QueryFirstAsync<int>("Select id From Application where applicationGuid=@guid", new { guid });
+                var idComment = await con.QueryFirstAsync<int>("Select id from ApplicationRatings where idUser=@idUser", new { idUser });
+                var rating = new ApplicationRatings_dbo()
+                {
+                    id=idComment,
+                    idUser = idUser,
+                    comment = comment.message,
+                    rating = comment.rating,
+                    idApplication = idApp
+                };
+                await con.UpdateAsync(rating);
                 con.Close();
             }
             return Ok();
